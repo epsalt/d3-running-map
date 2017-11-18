e9309a76648f58f3957fc5f574e8ac1fc45d6f38
epsalt <evan.saltman@gmail.com>
2017-11-18 16:50:00 -0700

    Intial running map implementation

---
+++ js/running_map.js
@@ -0,0 +1,65 @@
+var pi = Math.PI,
+    tau = 2 * pi;
+
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var projection = d3.geoMercator()
+    .scale((1 << 21) / tau)
+    .translate([width / 2, height / 2])
+    .center([-114.07, 51.05]);
+
+var path = d3.geoPath()
+    .projection(projection);
+
+var tiles = d3.tile()
+    .size([width, height])
+    .scale(projection.scale() * tau)
+    .translate(projection([0, 0]))
+();
+
+d3.csv("data/gpx_rollup.csv", function(data) {
+
+    svg.selectAll("image")
+        .data(tiles)
+        .enter().append("image")
+        .attr("xlink:href", function(d) { return "http://" + "abc"[d[1] % 3] + ".tile.openstreetmap.org/" + d[2] + "/" + d[0] + "/" + d[1] + ".png"; })
+        .attr("x", function(d) { return (d[0] + tiles.translate[0]) * tiles.scale; })
+        .attr("y", function(d) { return (d[1] + tiles.translate[1]) * tiles.scale; })
+        .attr("width", tiles.scale)
+        .attr("height", tiles.scale);
+
+    var line = d3.line()
+        .x(function(d) {return projection([d.lon, d.lat])[0];})
+        .y(function(d) {return projection([d.lon, d.lat])[1];});
+    
+    var wait = 5000, lastUpdate = -1000;
+    d3.timer(function(elapsed) {
+        if(elapsed - lastUpdate > wait) {
+            update(elapsed);
+            lastUpdate = elapsed;
+            console.log("UPDATE!");
+        }
+    });
+
+    function update(elapsed){
+
+        var dataByIndex = d3.nest()
+            .key(function(d) {return d.index;})
+            .entries(data.filter(function(d) {return d.elapsed < elapsed / 50;}));
+
+        svg.selectAll("path")
+            .remove();
+        
+        svg.selectAll("path")
+            .data(dataByIndex)
+            .enter().append("path")
+            .attr("d", function(d) {return line(d.values);})
+            .attr("id", function(d) {return d.key;})
+            .attr("stroke", "blue")
+            .attr("stroke-width", 2)
+            .attr("fill", "none");
+    }
+
+});
