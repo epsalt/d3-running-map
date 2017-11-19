e042b68933039d5011f23accaa35d8bc342543d0
epsalt <evan.saltman@gmail.com>
2017-11-19 13:16:40 -0700

    Add dynamic text element to display elapsed time

--- js/running_map.js
+++ js/running_map.js
@@ -54,12 +54,26 @@ d3.csv("data/gpx_rollup.csv", function(data) {
         .attr("fill", "none")
         .attr("r", 2);
 
-    var wait = 15, lastUpdate = -1000;
+    svg.append("rect")
+        .attr("x", 350)
+        .attr("y", 525)
+        .attr("width", 140)
+        .attr("height", 70)
+        .attr("fill", "#fff")
+        .attr("stroke", "#555")
+        .attr("stroke-width", 0.25);
+
+    svg.append("text")
+        .attr("x", 360)
+        .attr("y", 540)
+        .attr("class", "legend")
+        .text("Elapsed - 00:00:00");
+
+    var wait = 30, lastUpdate = -1000;
     d3.timer(function(elapsed) {
         if(elapsed - lastUpdate > wait) {
             update(elapsed, dataByIndex);
             lastUpdate = elapsed;
-            console.log("UPDATE!");
         }
     });
 
@@ -68,6 +82,12 @@ d3.csv("data/gpx_rollup.csv", function(data) {
         var elapsedFilter = data.filter(function(d) {return d.elapsed < elapsed / 10;}),
             nested = dataByIndex.entries(elapsedFilter),
             currentPoints = nested.map(function(d) {return d.values[d.values.length - 1];});
+
+        var date = new Date(null);
+        date.setSeconds(elapsed / 10);
+
+        svg.select("text")
+            .text("Elapsed " + date.toISOString().substr(11, 8));
 
         svg.selectAll("path")
             .data(nested)
